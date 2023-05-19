185184a680ed1b6242d2e0206e137017975412b7
Nikhil <98630750+Nikhil-0088@users.noreply.github.com>
2023-05-19 16:51:59 +0530

    Update conn.js
    changing the mongo db connection string

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect("mongodb://localhost:27017/contact",{
+mongoose.connect("mongodb+srv://nikhil_0088:<password>@cluster0.bmke4qv.mongodb.net/?retryWrites=true&w=majority",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
 }).then(()=>{
