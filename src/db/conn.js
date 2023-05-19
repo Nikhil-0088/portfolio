0005f60e4b7b4ead2e78817e20caa61ae1d5b8c2
Nikhil <98630750+Nikhil-0088@users.noreply.github.com>
2023-05-19 16:53:30 +0530

    Update conn.js
    adding the password to mongo db atlas

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect("mongodb+srv://nikhil_0088:<password>@cluster0.bmke4qv.mongodb.net/?retryWrites=true&w=majority",{
+mongoose.connect("mongodb+srv://nikhil_0088:Jayasri0088luster0.bmke4qv.mongodb.net/?retryWrites=true&w=majority",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
 }).then(()=>{
